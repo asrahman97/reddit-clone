1dcce98d7610b9d8092be36b56e11deb4817c17c
asrahman97 <asrahman97@gmail.com>
2019-12-12 11:02:07 -0500

    Initial commit after deleting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: '',
+      post: []
+
+    }
+  }
+
+
+  onChange = (e) => {
+    //completely optional variable but makes it easier
+    const content = e.target.value;
+    this.setState({
+      content:
+    })
+  }
+
+  postSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Reddit</h1>
+
+        <form onSubmit={this.postSubmit}>
+          <input type='textarea' name='content' value={this.state.content} placeholder='Post It' onChange={this.onChange} />
+          <input type='submit' value='post' />
+        </form>
+
+        Hello world
+        
     </div>
-  );
+    );
+  }
 }
 
 export default App;
